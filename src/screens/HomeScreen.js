5807fb094278d0d911ecf092dcc00bdff4e1edfe
Nabeel Anjum <nabeel@maavan.com>
2018-12-31 16:43:25 +0500

    Dashboard fixes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import FontAwesome, { Icons } from 'react-native-fontawesome';
 import {
   Platform,
   StyleSheet,
@@ -14,13 +13,13 @@ export default class App extends Component<{}> {
   render() {
     return(
       <View style={styles.container}>
-        <ScrollView>
-          <View style={styles.navbar}>
-            <View style={styles.afterImageView}>
-              <View><TouchableOpacity onPress={() => this.props.navigation.openDrawer()}><Image source={require('../images/menu_icon.png')}
-              resizeMode={'contain'} style={{width: 70, height: 50}}/></TouchableOpacity></View>
-            </View>
+        <View style={styles.navbar}>
+          <View style={styles.afterImageView}>
+            <View><TouchableOpacity onPress={() => this.props.navigation.openDrawer()}><Image source={require('../images/menu_icon.png')}
+            resizeMode={'contain'} style={{width: 70, height: 50}}/></TouchableOpacity></View>
           </View>
+        </View>
+        <ScrollView>
           <View style={styles.dashboardView}>
             <Text style={styles.dashText}>Dashboard</Text>
           </View>
@@ -90,6 +89,7 @@ export default class App extends Component<{}> {
 const styles = {
   container: {
     flex: 1,
+    backgroundColor: '#edf1f5',
   },
   navbar:{
     justifyContent: 'center',
@@ -100,20 +100,19 @@ const styles = {
     alignItems: 'flex-start'
   },
   dashboardView: {
-    backgroundColor: '#fff',
-    height: 60,
+    height: 70,
     justifyContent: 'center',
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   dashText: {
     color: '#2098d1',
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   tilesView: {
     backgroundColor: '#edf1f5',
     // paddingHorizontal: 10,
-    paddingVertical: 40
   },
   flexView: {
     flexDirection: 'row',
