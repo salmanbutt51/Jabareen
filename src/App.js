2dad67ddc911c28839e0c59f38c82359ae53d806
Nabeel Anjum <nabeel@maavan.com>
2019-01-17 02:10:23 +0500

    Video and map added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react';
-// import Video from 'react-native-video';
+import Video from 'react-native-video';
 import {
   Platform,
   StyleSheet,
-  Text,
   View,
 }
 from 'react-native';
-// import VideoScreen from './screens/VideoScreen';
 import AppContainer from './utils/router';
 export default class App extends Component<{}> {
+
+  state = {
+    videoEnded: false
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <AppContainer />
+      {
+        this.state.videoEnded
+        ? <AppContainer />
+        : <Video source={require('./videos/video.mp4')}   // Can be a URL or a local file.
+           ref={(ref) =>
+             this.player = ref
+           }
+           onError={() => console.log('Video was not loaded')}
+           style={styles.backgroundVideo}
+           resizeMode='cover'
+           onEnd={() => this.setState({videoEnded: true})}
+        />
+      }
       </View>
     );
   }
@@ -22,5 +37,12 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
 });
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
   View,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  Button,
-  AsyncStorage,
-  FlatList
 } from 'react-native';
 import Header from '../components/Header';
 // import services from '../utils/services';
+import MapView from 'react-native-maps';
 export default class App extends Component<{}> {
 
   render() {
     return(
       <View style={styles.container}>
         <Header navigation={this.props.navigation} title={'Company Map'}/>
-
+        <MapView
+          style={{flex: 1}}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
@@ -29,5 +29,4 @@ const styles = {
     flex: 1,
     backgroundColor: '#edf1f5',
   },
-
-}
+};
