c0548825a11e0fbb6e84094bca3218741b10522a
salmanbutt51 <41136972+salmanbutt51@users.noreply.github.com>
2019-02-09 03:47:49 +0500

    profile screen

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -6,20 +6,53 @@ import {
   TouchableOpacity,
   AsyncStorage,
   FlatList,
-  TextInput
+  TextInput,
+  ScrollView,
+  Image,
+  ImageBackground
 } from 'react-native';
 import services from '../utils/services';
 import Header from '../components/Header';
 import DropdownMessageAlert from '../templates/DropdownMessageAlert';
+import LoadingButton from '../templates/LoadingButton';
 export default class App extends Component<{}> {
 
   render() {
     return(
       <View style={styles.mainContainer}>
         <Header navigation={this.props.navigation} showUserIcon={false} title={'Profile Overview'} />
-        <View style={styles.container}>
-
-        </View>
+        <ScrollView>
+          <ImageBackground source={require('../images/profile-pic.png')} style={{flex: 1}}>
+            <View style={styles.picView}>
+              <Image
+                source={require('../images/profile-pic.png')}
+                resizeMode={'contain'}
+                style={styles.profilePic}
+              />
+              <Text style={[styles.picText, {marginTop: 10}]}>Change photo</Text>
+              <Text style={[styles.picText, {fontSize: 18, marginVertical: 5}]}>Muhammad</Text>
+              <Text style={styles.picText}>sbg.med5@gmail.com</Text>
+            </View>
+          </ImageBackground>
+          <View style={styles.container}>
+            <Text style={styles.inputText}>Full Name</Text>
+            <TextInput style={styles.inputBox}
+            value={'Muhammad'}
+              placeholderTextColor = "#a6b8d4"
+            />
+            <Text style={styles.inputText}>Full Name</Text>
+            <TextInput style={styles.inputBox}
+            value={'Address'}
+              placeholderTextColor = "#a6b8d4"
+            />
+            <Text style={styles.inputText}>Mobile<Text style={{color: 'red'}}>*</Text></Text>
+            <TextInput style={styles.inputBox}
+            value={'042435546 '}
+              placeholderTextColor = "#a6b8d4"
+            />
+            <LoadingButton title='Update Profile' titleStyle={{fontSize: 18}} style={styles.button} />
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -31,8 +64,47 @@ const styles = {
     flex: 1,
     backgroundColor: '#edf1f5',
   },
-  container: {
-    padding: 10
+  picView: {
+    height: 230,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#7c87d4'
   },
-
+  profilePic: {
+    height: 100,
+    borderRadius: 70
+  },
+  picText: {
+    color: '#fff',
+    fontSize: 15
+  },
+  container: {
+    padding: 10,
+    marginTop: 20
+  },
+  inputText:{
+    fontSize: 18,
+    color: 'black',
+    paddingLeft: 5
+  },
+  inputBox: {
+    borderBottomWidth: 1,
+    borderColor: '#a6b8d4',
+    fontSize: 18,
+    color: '#a4a4a4',
+    marginTop: 10,
+    marginBottom: 20,
+    // paddingHorizontal: 8,
+    height: 40,
+  },
+  button: {
+    // flex: 1,
+    backgroundColor: '#f33155',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    width: '100%',
+    borderRadius: 5
+    // paddingVertical: 30
+  },
 };
